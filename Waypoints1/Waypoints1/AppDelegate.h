5a66bbb758b605905570e18e6c660f38e8ee29d7
arielm <ariel@chronotext.org>
2018-05-21 14:06:33 +0300

    Starting with Waypoints1 project

---
+++ Waypoints1/Waypoints1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Waypoints1
+//
+//  Created by Ariel Malka on 21/05/2018.
+//  Copyright © 2018 Dronomy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
