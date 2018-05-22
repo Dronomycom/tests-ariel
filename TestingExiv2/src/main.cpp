d934d469383454da040f76c86889f0c63bc44554
arielm <ariel@chronotext.org>
2018-05-22 17:42:28 +0300

    TestingExiv2: Simplification of the problem

--- TestingExiv2/src/main.cpp
+++ TestingExiv2/src/main.cpp
@@ -16,28 +16,33 @@ int main(int argc, char** argv)
 //  Exiv2::ExifData exifData;
 //  exifData["Exif.Image.XResolution"] = Exiv2::FloatValue(123.456f); // FloatValue
 //  exifData["Exif.Image.YResolution"] = Exiv2::Rational(-2, 3); // RationalValue
-//  exifData["Exif.GPSInfo.GPSLatitude"] = Exiv2::FloatValue(32.1662).toRational(); // 32/1 9/1 146003/2500
+//  exifData["Exif.GPSInfo.GPSLatitude"] = Exiv2::FloatValue(32.1662).toRational(); // Exiv2::floatToRationalCast(32.1662)
 //
 //  cout << exifData["Exif.Image.XResolution"].value().toFloat() << endl;
 //  cout << exifData["Exif.Image.YResolution"].value().toFloat() << endl;
 //  cout << exifData["Exif.GPSInfo.GPSLatitude"].value() << endl;
 
 
-  Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open("/Users/roikr/Downloads/ADJI_0014 - DJI_0021.jpg");
-  assert(image.get() != 0);
-  image->readMetadata();
+  Exiv2::URationalValue::AutoPtr rv(new Exiv2::URationalValue);
+  rv->read("32/1 9/1 146003/2500");
+  cout << (*rv).toFloat() << endl;
 
-  Exiv2::ExifData &exifData = image->exifData();
-  if (exifData.empty()) {
-    std::string error(argv[1]);
-    error += ": No Exif data found in the file";
-    throw Exiv2::Error(1, error);
-  }
 
-  /*
-   * Problem: it reads "32" instead of ~32.1662 (input is "32/1 9/1 146003/2500")
-   */
-  cout << "Latitude: " << exifData["Exif.GPSInfo.GPSLatitude"].value().toFloat() << endl;
+//  Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open("/Users/roikr/Downloads/ADJI_0014 - DJI_0021.jpg");
+//  assert(image.get() != 0);
+//  image->readMetadata();
+//
+//  Exiv2::ExifData &exifData = image->exifData();
+//  if (exifData.empty()) {
+//    std::string error(argv[1]);
+//    error += ": No Exif data found in the file";
+//    throw Exiv2::Error(1, error);
+//  }
+//
+//  /*
+//   * Problem: it reads "32" instead of ~32.1662 (input is "32/1 9/1 146003/2500")
+//   */
+//  cout << "Latitude: " << exifData["Exif.GPSInfo.GPSLatitude"].value().toFloat() << endl;
 
   return 0;
 }
