62d58db7be6ffec6cdd448fea51ab55d8c84fa6c
arielm <ariel@chronotext.org>
2018-05-22 18:05:57 +0300

    TestingExiv2: Parsing exif gps coordinates (read)
    TODO: write

--- TestingExiv2/src/main.cpp
+++ TestingExiv2/src/main.cpp
@@ -11,6 +11,24 @@
 
 using namespace std;
 
+/*
+ * http://dev.exiv2.org/boards/3/topics/3132?r=3134
+ */
+double gpsRationalToDecimal(Exiv2::Metadatum& value)
+{
+  double decimal = 0 ;
+  double denom   = 1 ;
+
+  for ( int i = 0 ; i < value.count() ; i++)
+  {
+    Exiv2::Rational rational = value.toRational(i);
+    decimal += value.toFloat(i) / denom;
+    denom   *= 60;
+  }
+
+  return decimal;
+}
+
 int main(int argc, char** argv)
 {
 //  Exiv2::ExifData exifData;
@@ -23,26 +41,26 @@ int main(int argc, char** argv)
 //  cout << exifData["Exif.GPSInfo.GPSLatitude"].value() << endl;
 
 
-  Exiv2::URationalValue::AutoPtr rv(new Exiv2::URationalValue);
-  rv->read("32/1 9/1 146003/2500");
-  cout << (*rv).toFloat() << endl;
+//  Exiv2::URationalValue::AutoPtr rv(new Exiv2::URationalValue);
+//  rv->read("32/1 9/1 146003/2500");
+//  cout << (*rv).toFloat() << endl;
 
 
-//  Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open("/Users/roikr/Downloads/ADJI_0014 - DJI_0021.jpg");
-//  assert(image.get() != 0);
-//  image->readMetadata();
-//
-//  Exiv2::ExifData &exifData = image->exifData();
-//  if (exifData.empty()) {
-//    std::string error(argv[1]);
-//    error += ": No Exif data found in the file";
-//    throw Exiv2::Error(1, error);
-//  }
-//
-//  /*
-//   * Problem: it reads "32" instead of ~32.1662 (input is "32/1 9/1 146003/2500")
-//   */
-//  cout << "Latitude: " << exifData["Exif.GPSInfo.GPSLatitude"].value().toFloat() << endl;
+  Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open("/Users/roikr/Downloads/ADJI_0014 - DJI_0021.jpg");
+  assert(image.get() != 0);
+  image->readMetadata();
+
+  Exiv2::ExifData &exifData = image->exifData();
+  if (exifData.empty()) {
+    std::string error(argv[1]);
+    error += ": No Exif data found in the file";
+    throw Exiv2::Error(1, error);
+  }
+
+  /*
+   * Parses "32/1 9/1 146003/2500" into 32.1662
+   */
+  cout << "Latitude: " << gpsRationalToDecimal(exifData["Exif.GPSInfo.GPSLatitude"]) << endl;
 
   return 0;
 }
