10d23e1c3b027be47c954f62b77c34d5b06658ec
arielm <ariel@chronotext.org>
2018-07-25 18:52:48 +0300

    Logging1: Testing NSOperation

--- Logging1/src/ofApp.mm
+++ Logging1/src/ofApp.mm
@@ -2,6 +2,23 @@
 
 void ofApp::setup()
 {
+    NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
+    operationQueue.maxConcurrentOperationCount = 1;
+    
+    NSBlockOperation *operation1 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"OPERATION 1 BEGIN");
+        sleep(2);
+        NSLog(@"OPERATION 1 END");
+    }];
+    
+    NSBlockOperation *operation2 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"OPERATION 2 BEGIN");
+        sleep(1);
+        NSLog(@"OPERATION 2 END");
+    }];
+
+    [operationQueue addOperation:operation1];
+    [operationQueue addOperation:operation2];
 }
 
 void ofApp::draw()
