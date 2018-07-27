b317383248b79751e37ba3dc0f1a724ebe818c35
arielm <ariel@chronotext.org>
2018-07-28 02:49:53 +0300

    Logging1: Uploader core in place. TODO: flushing

--- Logging1/src/Uploader.mm
+++ Logging1/src/Uploader.mm
@@ -30,20 +30,81 @@
         
         //
         
-        cout << "TODO: process " << line << endl;
+        cout << "PROCESSING " << line << endl;
+        
+        NSMutableDictionary *object = [[NSMutableDictionary alloc] init];
+        object[@"version"] = @"1.0";
+        object[@"messages"] = [[NSMutableArray alloc] init];
+        
+        ifstream input2(ofxiOSGetDocumentsDirectory() + line);
+        
+        string line2;
+        while (getline(input2, line2))
+        {
+            NSMutableDictionary *message = [[NSMutableDictionary alloc] init];
+            
+            istringstream iss(line2);
+            int type;
+            iss >> type;
+            
+            switch (type)
+            {
+                case 1:
+                {
+                    string product;
+                    double price;
+                    iss >> product >> price;
+                    
+                    message[@"product"] = ofxStringToNSString(product);
+                    message[@"price"] = [NSNumber numberWithDouble:price];
+                    break;
+                }
+                    
+                case 2:
+                {
+                    int age;
+                    string name;
+                    string surname;
+                    iss >> age >> name >> surname;
+                    
+                    message[@"age"] = [NSNumber numberWithInt:age];
+                    message[@"name"] = ofxStringToNSString(name);
+                    message[@"surname"] = ofxStringToNSString(surname);
+                    break;
+                }
+            }
+            
+            [object[@"messages"] addObject:message];
+            
+            if ([object[@"messages"] count] == 2)
+            {
+                [self flush:object];
+                [object[@"messages"] removeAllObjects];
+            }
+        }
+        
+        if ([object[@"messages"] count] > 0)
+        {
+            [self flush:object];
+        }
         
         //
         
         ofstream output(path);
-        for (auto &line2 : lines)
+        for (auto &line3 : lines)
         {
-            output << line2 << endl;
+            output << line3 << endl;
         }
         output.close();
-        
+
         NSError *error = nil;
         [[NSFileManager defaultManager] removeItemAtPath:ofxStringToNSString(ofxiOSGetDocumentsDirectory() + line) error:&error];
     }
+}
+
+- (void)flush:(NSDictionary*)object
+{
+    NSLog(@"*** FLUSHING: %@ ***", object);
 }
 
 @end
