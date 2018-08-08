3321a2d6657d7b0f49344cd5c4891ee16cfdade1
arielm <ariel@chronotext.org>
2018-08-08 12:52:55 +0300

    Logging2: Executing a simple task upon entering background
    The time allocated by the system until expiration is of 3 minutes

--- Logging2/src/AppDelegate.m
+++ Logging2/src/AppDelegate.m
@@ -22,4 +22,41 @@
     return YES;
 }
 
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    
+    __block UIBackgroundTaskIdentifier background_task;
+    
+    //Registered a background task, telling the system we need to borrow some events to the system
+    background_task = [application beginBackgroundTaskWithExpirationHandler:^ {
+        
+        //Whether or not complete, the end of background_task task
+        [application endBackgroundTask: background_task];
+        background_task = UIBackgroundTaskInvalid;
+    }];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        //### background task starts
+        NSLog(@"Running in the background\n");
+        
+        int i = 0;
+        while(i < 5)
+        {
+            NSLog(@"Background time Remaining: %f", [[UIApplication sharedApplication] backgroundTimeRemaining]); // See https://stackoverflow.com/a/48300278/50335
+            [NSThread sleepForTimeInterval:1]; //wait for 1 sec
+            i++;
+        }
+        
+        [application endBackgroundTask: background_task];
+        background_task = UIBackgroundTaskInvalid;
+    });
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
 @end
