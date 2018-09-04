7146a2582c3d682393bd56c89e611dbf92776846
arielm <ariel@chronotext.org>
2018-09-04 17:10:57 +0300

    Checklist: showing

--- Checklist/Checklist/ViewController.m
+++ Checklist/Checklist/ViewController.m
@@ -14,16 +14,14 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    UINavigationController *viewController = (UINavigationController*)[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"Checklist"];
+    viewController.modalPresentationStyle = UIModalPresentationFormSheet;
+    viewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    [self presentViewController:viewController animated:YES completion:nil];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
