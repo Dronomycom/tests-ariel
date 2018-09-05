533cb9a24c7436380add826aac94bf34f9c0c2c8
arielm <ariel@chronotext.org>
2018-09-05 11:07:46 +0300

    Checklist: Starting with UICollectionView

---
+++ Checklist/Checklist/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  Checklist
+//
+//  Created by Ariel Malka on 04/09/2018.
+//  Copyright © 2018 Dronomy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
