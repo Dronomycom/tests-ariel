2f5f39b5fda4bde32b4a83f2c5c9cd5105979c33
arielm <ariel@chronotext.org>
2018-09-17 16:18:36 +0300

    Starting with GZipTest1 project

---
+++ GZipTest1/GZipTest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZipTest1
+//
+//  Created by Ariel Malka on 17/09/2018.
+//  Copyright © 2018 Dronomy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
